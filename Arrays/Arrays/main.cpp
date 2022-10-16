273e03aa01f0e54af6da19ad1edfea106b91b146
Clayman <Clayman@DESKTOP-DM8E77U>
2022-10-16 15:59:04 +0300

    Arrays base tasks

---
+++ Arrays/Arrays/main.cpp
@@ -0,0 +1,55 @@
+﻿//Arrays
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello Arrays" << endl;
+	const int SIZE = 5;
+	int arr[SIZE] = { 3, 5, 8 };
+	//arr[2] = 123;			//обращаемся к элементу массива на запись
+	//cout << arr[2] << endl;	//обращаемся к элементу массива на чтение
+
+	//Ввод элементов массива с клавиатуры:
+	cout << "Введите элементы массива: ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}
+
+	//Вывод элементов массива в прямом порядке:
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	//Вывод элементов массива в обратном порядке:
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	//Вычисление суммы элементов массива:
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива: " << sum << endl;
+	cout << "Среднее-арифметическое элементов массива: " << (double)sum / SIZE << endl;
+	cout << typeid(sum).name() << endl;
+
+	//Поиск минимального и максимального значения в массиве:
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+	cout << "Максимально значение в массиве: " << max << endl;
+}
